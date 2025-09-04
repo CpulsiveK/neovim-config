9f45fab66c845c879600e1e93a5623bc95b5240f
Clifford <impulsivecliff7@gmail.com>
2025-09-04 14:59:02 +0000

    enabled windows clipboard for neovim

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.number = true
 vim.g.mapleader = " "
+
+-- Always use system clipboard in WSL Neovim
+if vim.fn.system('uname -r'):match("microsoft") then
+  vim.opt.clipboard = "unnamedplus"
+end
+
