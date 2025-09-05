425f814b014d1e0017a5b55cb12da7c2211a0867
Clifford <impulsivecliff7@gmail.com>
2025-09-05 09:12:56 +0000

    removed depracted lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "yamlls" },
+        ensure_installed = { "lua_ls", "ts_ls", "yamlls" },
         automatic_installation = true
       })
     end
@@ -24,7 +24,7 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.yamlls.setup({
