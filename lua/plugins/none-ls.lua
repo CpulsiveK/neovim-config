75447217141ab841006cd6f9eb3776a91f0ba141
Clifford Kwakye <impulisvecliff7@gmail>
2025-09-14 06:30:34 -0700

    fixed ts/js linting issue

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,21 @@
 return {
   "nvimtools/none-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.prettier
-      }
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.formatting.jq"),
+        require("none-ls.code_actions.eslint"),
+        null_ls.builtins.formatting.prettier,
+      },
     })
 
-    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-  end
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
